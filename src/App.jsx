e2614ab15273a3ba51a76aa00646ef7a7707e7ff
JamesWoo <JamesWoo>
2022-10-14 22:10:46 +0900

    show video list with title

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './App.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos , setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyApTGco30ECCxJBaqMZ4HABgax1PdbtW2Q", 
+      requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[])
+
+  return (
+    <VideoList videos={videos} />
+  );
+}
+
+export default App;
