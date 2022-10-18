fd29b38e64452d66574546c8c34dba4a6e238af8
JamesWoo <JamesWoo>
2022-10-18 22:19:53 +0900

    add detail

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({ youtube }) {
   const [videos , setVideos] = useState([]);
+  const [selectedVideo , setSElectedVideo] = useState(null);
+
+  const selecteVideo = (video) =>{
+    setSElectedVideo(video);
+  }
   const search = query =>{
+    setSElectedVideo(null); // 검색후 다시 목록으로가기위해 사용
     youtube.search(query)
     .then(videos => setVideos(videos));
   }
@@ -20,7 +27,17 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selecteVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
